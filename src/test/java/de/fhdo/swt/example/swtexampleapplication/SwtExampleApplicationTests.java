fabd45af3260dc5b0701271eea532f4ce7f325a2
Jannled <Jannled@users.noreply.github.com>
2020-05-18 13:22:09 +0200

    Pushing the given Example

---
+++ src/test/java/de/fhdo/swt/example/swtexampleapplication/SwtExampleApplicationTests.java
@@ -0,0 +1,14 @@
+package de.fhdo.swt.example.swtexampleapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwtExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
