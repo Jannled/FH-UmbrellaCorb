8132101624afb99c69a01033480a47ca41370c72
Jannled <Jannled@live.de>
2020-06-08 16:21:44 +0200

    ReiseVerwaltung hinzugefügt

--- src/main/java/UmbrellaCorp/UmbrellaTravel/UmbrellaTravelApplication.java
+++ src/main/java/UmbrellaCorp/UmbrellaTravel/UmbrellaTravelApplication.java
@@ -2,10 +2,12 @@ package UmbrellaCorp.UmbrellaTravel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class UmbrellaTravelApplication {
-
+@EnableJpaRepositories("UmbrellaCorp.UmbrellaTravel.ReiseVerwaltung")
+public class UmbrellaTravelApplication 
+{
 	public static void main(String[] args) {
 		SpringApplication.run(UmbrellaTravelApplication.class, args);
 	}
