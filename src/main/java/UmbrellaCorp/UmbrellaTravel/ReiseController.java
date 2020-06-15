b156b654bfba7db4b03c1a7c2e5360e1d0b7e747
Jannled <Jannled@live.de>
2020-06-15 14:53:41 +0200

    Kurzer push ohne CrudRepository
    schmeißt die ganze Zeit
    Field reiseRepository in UmbrellaCorp.UmbrellaTravel.ReiseController required a bean of type 'UmbrellaCorp.UmbrellaTravel.ReiseRepository' that could not be found.

--- src/main/java/UmbrellaCorp/UmbrellaTravel/ReiseController.java
+++ src/main/java/UmbrellaCorp/UmbrellaTravel/ReiseController.java
@@ -4,17 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ReiseController 
 {
-	@Autowired
-	private ReiseRepository reiseRepository;
+	//@Autowired
+	//private ReiseRepository reiseRepository;
 
 	@GetMapping("/reisen")
 	public String reisenRequest(Reise reise, Model model) 
 	{
-		model.addAttribute("journeys", reiseRepository.findAll());
+	//	model.addAttribute("reisen", reiseRepository.findAll());
 		return "reise_buchen";
 	}
 }
