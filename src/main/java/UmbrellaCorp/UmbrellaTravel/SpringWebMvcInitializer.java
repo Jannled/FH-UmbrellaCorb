1c5a507ea61c0f6804bc09e42c12a2adf63f25d0
jesch029 <epsilon96@gmx.de>
2020-06-29 14:18:12 +0200

    MVCInit Klasse

---
+++ src/main/java/UmbrellaCorp/UmbrellaTravel/SpringWebMvcInitializer.java
@@ -0,0 +1,22 @@
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringWebMvcInitializer extends
+   AbstractAnnotationConfigDispatcherServletInitializer {
+
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[] { WebSecurityConfig.class };
+  }
+
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected String[] getServletMappings() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
